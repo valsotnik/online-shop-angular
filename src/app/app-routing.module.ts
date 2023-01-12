fe6be6f093b93df5fbb91c93a3d367fd98176623
valery.sotnik <cmvaltime@gmail.com>
2023-01-12 11:41:50 +0100

    init: initialize project and base configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {BasketComponent} from './components/basket/basket.component'
+import {ProductDetailsComponent} from './components/product-details/product-details.component'
+import {ProductsComponent} from './components/products/products.component'
+import {BaseComponent} from './components/base/base.component'
+import {NgModule} from '@angular/core'
+import {RouterModule, Routes} from '@angular/router'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BaseComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'product/:id', component: ProductDetailsComponent},
+  {path: 'basket', component: BasketComponent},
+
+  {path: '**', redirectTo: '', component: BaseComponent},
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module'
+import {AppComponent} from './app.component'
+import {ProductsComponent} from './components/products/products.component'
+import {BasketComponent} from './components/basket/basket.component'
+import {ProductDetailsComponent} from './components/product-details/product-details.component'
+import {BaseComponent} from './components/base/base.component'
+import {UiModule} from './components/UI/ui/ui.module'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    BasketComponent,
+    ProductDetailsComponent,
+    BaseComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, UiModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
